a25783c48856078c891cedf640e5aa865d243896
Emilio Alonso <emilioalonso1981@gmail.com>
2021-05-20 16:39:33 +0200

    Classes nad members

---
+++ SlidesStaticImport/src/com/pluralsight/slidesstaticimport/Flight.java
@@ -0,0 +1,49 @@
+package com.pluralsight.slidesstaticimport;
+
+public class Flight {
+    private int passengers, seats = 150;
+    private static int allPassengers;
+    private static int maxPassengersPerFlight;
+
+    static {
+        AdminService admin = new AdminService();
+        admin.connect();
+
+        maxPassengersPerFlight = admin.isRestricted() ?
+                admin.getMaxFlightPassengers() : Integer.MAX_VALUE;
+        //System.out.println(maxPassengersPerFlight);
+
+        admin.close();
+        System.out.println("executing static initialization block");
+
+    }
+
+    public static int getAllPassengers() {
+        return allPassengers;
+    }
+
+    public static void resetAllPassengers() {
+        allPassengers = 0;
+    }
+
+    public void add1Passenger() {
+        if(passengers < seats && passengers < maxPassengersPerFlight) {
+            passengers += 1;
+            allPassengers += 1;
+        }
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+
+        this.seats = seats;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+}
