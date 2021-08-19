637bd202afe5dc3fec8120a82b609afca4ee8f5e
EmilioAlonsoN <42852279+EmilioAlonsoN@users.noreply.github.com>
2021-08-19 12:01:43 +0200

    Delete conference-demo directory

--- conference-demo/src/main/java/com/emilio/conferencedemo/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emilio.conferencedemo.repositories;
-
-import com.emilio.conferencedemo.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-}
