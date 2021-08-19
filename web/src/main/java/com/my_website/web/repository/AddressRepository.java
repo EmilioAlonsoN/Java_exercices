8accb62beefc4bd893ce99003386c23a87928b1d
EmilioAlonsoN <42852279+EmilioAlonsoN@users.noreply.github.com>
2021-08-19 11:58:56 +0200

    Delete web directory

--- web/src/main/java/com/my_website/web/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.my_website.web.repository;
-
-import com.my_website.web.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- web/src/main/java/com/my_website/web/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.my_website.web.repository;
-
-import com.my_website.web.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
